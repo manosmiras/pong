f30d29cc5f9009b43b8ecd46c5d9b8b1481385f6
Manos Miras <manosmiras@outlook.com>
2023-11-12 02:32:33 +0200

    feat: player movement / pong physics

--- main.js
+++ main.js
@@ -5,51 +5,200 @@ const app = new PIXI.Application({
     resizeTo: window,
 });
 
+class Entity extends PIXI.Sprite{
+    move(){
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+};
+
+class Ball extends Entity {
+    constructor() {
+        super(PIXI.Texture.from('square.png'));
+        // center the sprite's anchor point
+        this.anchor.set(0.5);
+        // move the sprite to the center of the screen
+        this.x = app.screen.width / 2;
+        this.y = app.screen.height / 2;
+        // set velocity
+        this.vx = 0;
+        this.vy = 0;
+        // set scale
+        this.scale.x = 0.1;
+        this.scale.y = 0.1;
+    }
+
+    reset() {
+        this.vx = Math.random() < 0.5 ? Math.floor(Math.random() * 2) - 3 : Math.floor(Math.random() * 2) + 2;
+        this.vy = Math.random() < 0.5 ? Math.floor(Math.random() * 2) - 3 : Math.floor(Math.random() * 2) + 2;
+        console.log("Setting velocity to: " + ball.vx + " " + ball.vy);
+        this.x = app.screen.width / 2;
+        this.y = app.screen.height / 2;
+    }
+
+    hasHitPaddle(paddle){
+        let max = paddle.y + paddle.height / 2;
+        let min = paddle.y - paddle.height / 2;
+    
+        if(this.y > min && this.y < max){
+            var dx = paddle.x - this.x;
+            if(dx * dx < 50){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+class Paddle extends Entity {
+    constructor(x) {
+        super(PIXI.Texture.from('square.png'));
+        // center the sprite's anchor point
+        this.anchor.set(0.5);
+        // set position
+        this.x = x;
+        this.y = app.screen.height / 2;
+        // set velocity
+        this.vx = 0;
+        this.vy = 0;
+        // set scale
+        this.scale.x = 0.1;
+        this.scale.y = 1;
+    }
+};
+
 document.body.appendChild(app.view);
 
-const ball = createBall();
-const playerPaddle = createPaddle(0);
-const aiPaddle = createPaddle(app.screen.width);
+const leftLimit = app.screen.width / 8;
+const rightLimit = app.screen.width - app.screen.width / 8;
+let ball = new Ball();
+let playerPaddle = new Paddle(leftLimit);
+let aiPaddle = new Paddle(rightLimit);
 
+let playerScore = createText(0, app.screen.width / 3, app.screen.height / 6, 72);
+let aiScore = createText(0, app.screen.width - app.screen.width / 3, app.screen.height / 6, 72);
 // add to stage
+app.stage.addChild(aiScore);
+app.stage.addChild(playerScore);
 app.stage.addChild(ball);
 app.stage.addChild(playerPaddle);
 app.stage.addChild(aiPaddle);
-ball.vx = 5;
+
+ball.reset();
+let entities = [ball, playerPaddle, aiPaddle];
+
+const up = keyboard("ArrowUp"), down = keyboard("ArrowDown");
+
+up.press = () => {
+    playerPaddle.vy = -5;
+};
+
+up.release = () => {
+    if (!down.isDown) {
+        playerPaddle.vy = 0;
+    }
+};
+
+down.press = () => {
+    playerPaddle.vy = 5;
+};
+down.release = () => {
+    if (!up.isDown) {
+        playerPaddle.vy = 0;
+    }
+};
+
+const padding = 20;
 
 app.ticker.add(() => {
-    if(ball.x >= app.screen.width){
-        ball.vx = -5;
-    } else if (ball.x <= 0){
-        ball.vx = 5;
+    if(ball.y > app.screen.height || ball.y < 0){
+        ball.vy = -ball.vy;
     }
-    ball.x += ball.vx;
+
+    if (ball.hasHitPaddle(playerPaddle)) {
+        ball.vx = -ball.vx;
+    }
+    if (ball.hasHitPaddle(aiPaddle)) {
+        ball.vx = -ball.vx;
+    }
+    if (hasPlayerScored(ball, rightLimit + padding)) {
+        handlePlayerScored();
+    } else if (hasAIScored(ball, leftLimit - padding)) {
+        handleAIScored();
+    }
+    entities.forEach(entity => {
+        entity.move();
+    });
 });
 
-function createBall(){
-    // create a new Sprite from an image path
-    const ball = PIXI.Sprite.from('https://pixijs.com/assets/star.png');
-    // center the sprite's anchor point
-    ball.anchor.set(0.5);
-    // move the sprite to the center of the screen
-    ball.x = app.screen.width / 2;
-    ball.y = app.screen.height / 2;
-    ball.scale.x = 0.1;
-    ball.scale.y = 0.1;
-    return ball;
+function hasPlayerScored(ball, limit) {
+    return ball.x >= limit;
 }
 
-function createPaddle(x){
-    // create a sprite
-    const paddle = PIXI.Sprite.from('https://pixijs.com/assets/star.png');
-    // center the sprite's anchor point
-    paddle.anchor.set(0.5);
-    // set position
-    paddle.x = x;
-    paddle.y = app.screen.height / 2;
-    // set scale
-    paddle.scale.x = 0.1;
-    paddle.scale.y = 1;
+function hasAIScored(ball, limit) {
+    return ball.x <= limit;
+}
 
-    return paddle;
+function handlePlayerScored() {
+    ball.reset();
+    playerScore.text++;
+}
+
+function handleAIScored() {
+    ball.reset();
+    aiScore.text++
+}
+
+function createText(content, x, y, fontSize) {
+    const text = new PIXI.Text(content, { fontFamily: 'Arial', fontSize: fontSize, fill: 0xffffff });
+    text.x = x - fontSize + fontSize / 2;
+    text.y = y - fontSize / 2;
+    return text;
+}
+
+function keyboard(value) {
+    const key = {};
+    key.value = value;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    // The `downHandler`
+    key.downHandler = (event) => {
+        if (event.key === key.value) {
+            if (key.isUp && key.press) {
+                key.press();
+            }
+            key.isDown = true;
+            key.isUp = false;
+            event.preventDefault();
+        }
+    };
+
+    // The `upHandler`
+    key.upHandler = (event) => {
+        if (event.key === key.value) {
+            if (key.isDown && key.release) {
+                key.release();
+            }
+            key.isDown = false;
+            key.isUp = true;
+            event.preventDefault();
+        }
+    };
+
+    // Attach event listeners
+    const downListener = key.downHandler.bind(key);
+    const upListener = key.upHandler.bind(key);
+
+    window.addEventListener("keydown", downListener, false);
+    window.addEventListener("keyup", upListener, false);
+
+    // Detach event listeners
+    key.unsubscribe = () => {
+        window.removeEventListener("keydown", downListener);
+        window.removeEventListener("keyup", upListener);
+    };
+
+    return key;
 }
